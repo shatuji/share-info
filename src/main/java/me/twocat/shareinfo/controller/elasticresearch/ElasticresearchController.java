340932c41bb7078742710eb02d83ea737c891fd7
wangyongqiang <pinewang@foxmail.com>
2020-02-19 20:51:53 +0800

    001

--- src/main/java/me/twocat/shareinfo/controller/elasticresearch/ElasticresearchController.java
+++ src/main/java/me/twocat/shareinfo/controller/elasticresearch/ElasticresearchController.java
@@ -1,8 +1,19 @@
 package me.twocat.shareinfo.controller.elasticresearch;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.ws.Response;
+
+@RestController
+@RequestMapping("/elatc")
 public class ElasticresearchController {
 
+  @RequestMapping("/test")
+  public ResponseEntity test(){
+    return null ;
+  }
 
 }
