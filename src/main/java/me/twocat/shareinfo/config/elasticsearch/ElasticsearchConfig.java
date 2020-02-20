1d9ecf8e4ecc76c4b73c3d300444c6ddc1851a69
wangyongqiang <pinewang@foxmail.com>
2020-02-20 23:23:37 +0800

    demo elasticsearch

--- src/main/java/me/twocat/shareinfo/config/elasticsearch/ElasticsearchConfig.java
+++ src/main/java/me/twocat/shareinfo/config/elasticsearch/ElasticsearchConfig.java
@@ -1,7 +1,13 @@
 package me.twocat.shareinfo.config.elasticsearch;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,17 +16,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticsearchConfig {
     @Value("${elasticsearch.host}")
-    String elasticsearch;
-
+    private  String host;
+    @Value("${elasticsearch.port}")
+    private int port;
+    @Value("${elasticsearch.username}")
+    private String userName;
+    @Value("${elasticsearch.password}")
+    private String password;
     /**
      * let’s create a config folder and create a class called ElasticsearchConfiguration.
      * @return
      */
     @Bean(destroyMethod = "close")
     public RestHighLevelClient client(){
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost(elasticsearch))
-        );
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY ,
+          new UsernamePasswordCredentials(userName , password));
+
+      RestClientBuilder builder = RestClient.builder(new HttpHost(host , port))
+        .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+      RestHighLevelClient client = new RestHighLevelClient(builder);
+
         return client;
     }
 }
--- src/main/java/me/twocat/shareinfo/entity/elasticsearchtest/ProfileDocument.java
+++ src/main/java/me/twocat/shareinfo/entity/elasticsearchtest/ProfileDocument.java
@@ -1,7 +1,10 @@
 package me.twocat.shareinfo.entity.elasticsearchtest;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class ProfileDocument {
 
 
